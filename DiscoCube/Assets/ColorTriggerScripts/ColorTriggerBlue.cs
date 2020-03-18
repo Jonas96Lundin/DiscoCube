d141c8d540a825e98e2a2f53068290e707ad3725
Jonas96Lundin <62259168+Jonas96Lundin@users.noreply.github.com>
2020-03-18 16:38:07 +0100

    Grunderna för spelarkuben

---
+++ DiscoCube/Assets/ColorTriggerScripts/ColorTriggerBlue.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+public class ColorTriggerBlue : MonoBehaviour
+{
+
+    void OnTriggerEnter()
+    {
+        FindObjectOfType<ColorManager>().currentColor = ColorManager.Colors.blue;
+        FindObjectOfType<ColorManager>().CheckColorCollision();
+    }
+}
