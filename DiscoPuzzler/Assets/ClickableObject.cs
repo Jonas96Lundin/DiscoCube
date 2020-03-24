dff328a41c13f7637884185d09574952ed1009df
KarateRai <Raimon420@gmail.com>
2020-03-24 11:26:47 +0100

    Första jobbet
    La till scripts för att starta eventuell bana och en "start knapp" i form av en kub som blinkar

---
+++ DiscoPuzzler/Assets/ClickableObject.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class ClickableObject : MonoBehaviour
+{
+    private GameObject definedButton;
+    public UnityEvent OnClick = new UnityEvent();
+    public Animator anim;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        definedButton = this.gameObject;
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnMouseEnter()
+    {
+        if (null != anim)
+        {
+            anim.Play("CubeAnimation");
+        }
+    }
+    private void OnMouseOver()
+    {
+        
+    }
+
+    private void OnMouseExit()
+    {
+        if (null != anim)
+        {
+            anim.Play("CubeStill");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit Hit;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            if (Physics.Raycast(ray, out Hit) && Hit.collider.gameObject == gameObject)
+            {
+                Debug.Log("Hit cube.");
+                OnClick.Invoke();
+            }
+        }
+    }
+}
