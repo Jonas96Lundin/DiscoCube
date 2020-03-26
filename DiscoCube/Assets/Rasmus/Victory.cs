9ff5a084bbac39ec52850e6fa717d6ebe4827748
RasmusHellkvist <RasmusHellkvist@users.noreply.github.com>
2020-03-26 09:44:51 +0100

    Sweeeet victory.

---
+++ DiscoCube/Assets/Rasmus/Victory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Victory : MonoBehaviour
+{
+    public int score;
+
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        AddScore();
+    }
+    void AddScore()
+    {
+        score++;
+    }
+}
