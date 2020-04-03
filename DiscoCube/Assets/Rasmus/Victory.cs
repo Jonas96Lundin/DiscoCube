c33589e167d8d7bb174c95ac7f197066578da4b2
Zippsson <62258314+Zippsson@users.noreply.github.com>
2020-04-03 17:02:31 +0200

    Added FlipTrigger to Kristian Scene

--- DiscoCube/Assets/Rasmus/Victory.cs
+++ DiscoCube/Assets/Rasmus/Victory.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class Victory : MonoBehaviour
 {
-
-    
-
     public void OnTriggerEnter(Collider Goal)
     {
+
         Win();
     }
     public GameObject completeLevelUI;
