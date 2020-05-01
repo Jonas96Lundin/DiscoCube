e4ea9c151ce383309b20b45b95bce7bdcfd3ea09
Zippsson <62258314+Zippsson@users.noreply.github.com>
2020-05-01 11:42:09 +0200

    Controller setup fixes and deleted some unused assets

--- DiscoCube/Assets/Scripts/Kristian/ControllerSetup.cs
+++ DiscoCube/Assets/Scripts/Kristian/ControllerSetup.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class ControllerSetup : MonoBehaviour
 {
     Movement_Side_Change moveScript;
@@ -11,6 +10,7 @@ public class ControllerSetup : MonoBehaviour
     private bool xboxActivated, pS4Activated;
 
     private string xboxVertical, xboxHorizontal, xboxRSVertical, xboxRSHorizontal, ps4Vertical, ps4Horizontal, ps4RSVertical, ps4RSHorizontal;
+
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +27,7 @@ public class ControllerSetup : MonoBehaviour
         ps4Horizontal = "PS4 Horizontal";
         ps4RSVertical = "PS4 RS Vertical";
         ps4RSHorizontal = "PS4 RS Horizontal";
+
     }
 
     // Update is called once per frame
@@ -46,12 +47,13 @@ public class ControllerSetup : MonoBehaviour
             return;
     }
 
+    //TODO: Fix it so that when you have both types of controllers plugged in. They will not interfere with one another. 
     public void XboxControlConfiguration()
     {
         moveScript.inputVertical = xboxVertical;
         moveScript.inputHorizontal = xboxHorizontal;
         cameraScript.inputRSVertical = xboxRSVertical;
-        cameraScript.inputRSHorizontal = xboxRSHorizontal;
+        cameraScript.inputRSHorizontal = xboxRSHorizontal;      
     }
 
     public void PS4ControlConfiguration()
