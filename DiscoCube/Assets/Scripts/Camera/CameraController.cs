fb2ca5ffec4aae38af5150d2802479374c735567
Zippsson <62258314+Zippsson@users.noreply.github.com>
2020-05-22 17:12:49 +0200

    New victory screens

--- DiscoCube/Assets/Scripts/Camera/CameraController.cs
+++ DiscoCube/Assets/Scripts/Camera/CameraController.cs
@@ -39,7 +39,6 @@ public class CameraController : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(0) || Input.GetButtonDown("ActivateCamera"))
         {
-            Debug.Log("Freelook = " + freelookActivated);
             freelookActivated = true;
             localRotation = offset;
         }
