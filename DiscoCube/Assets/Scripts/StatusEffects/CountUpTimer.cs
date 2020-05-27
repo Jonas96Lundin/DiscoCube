44c0bf27fda0957867a01e781879d30ac1a30edd
Zippsson <62258314+Zippsson@users.noreply.github.com>
2020-05-27 14:06:56 +0200

    Unity Cleanup, added end music

---
+++ DiscoCube/Assets/Scripts/StatusEffects/CountUpTimer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CountUpTimer : MonoBehaviour
+{
+    private static bool isCounting = true;
+
+    private float timer = 0f;
+
+    [SerializeField]
+    Text timerCounterText;
+
+    void Start()
+    {
+        IsCounting = true;
+    }
+    public static bool IsCounting
+    {
+        set { isCounting = value; }
+        get { return isCounting; }
+    }
+
+    public float Timer
+    {
+        get { return timer; }
+    }
+    
+    
+    void Update()
+    {
+        if (isCounting == true)
+        {
+            timer += 1 * Time.deltaTime;
+            timerCounterText.text = timer.ToString("0");
+        }
+        
+    }
+}
