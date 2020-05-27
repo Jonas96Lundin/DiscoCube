dfe5e71f072880f0ae1320c0758a9ccbbb562462
KarateRai <Raimon420@gmail.com>
2020-05-27 11:18:14 +0200

    Seems legit

--- DiscoCube/Assets/Scripts/UI/ControllerDetection.cs
+++ DiscoCube/Assets/Scripts/UI/ControllerDetection.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.EventSystems;
 
 /// <summary>
 /// ControllerDetection checks when the first level is loaded if there is a controller connected. If there is, it shows a controller select overlay.
@@ -21,16 +18,16 @@ public class ControllerDetection : MonoBehaviour
 
     enum inputDevice { noController, xController, pController}
     static inputDevice currentInputDevice;
-   
+
+    string startScene = "Jonas Tutorial", mainMenuScene = "MainMenu";
     private void Awake()
     {
-        //Checks if the loaded scene is the first level of the game.
-        if (SceneManager.GetActiveScene().name == "Level1")
+        if(SceneManager.GetActiveScene().name == startScene)
             DetectIfControllerIsConnected();
+        //Checks if the loaded scene is the first level of the game and if the tutorial is skipped.
         //If an input device has been chosen in Levl1, then it shall keep displaying in the following levels.
-        else if (SceneManager.GetActiveScene().name != "Level1" || SceneManager.GetActiveScene().name != "MainMenu")
+        else if (SceneManager.GetActiveScene().name != mainMenuScene)
         {
-            Debug.Log(currentInputDevice);
             DisplayCorrectInputOverlay();
         }
         //This prevents the controller detection from displaying on the main menu.
@@ -44,29 +41,25 @@ public class ControllerDetection : MonoBehaviour
         for (int x = 0; x < names.Length; x++)
         {
             print(names[x].Length);
-            if (names[x].Length == 19)
+            if (names[x].Length == 19)//19 equals the number of characters in Playstation 4 controllers Unity name.
             {
                 controllerDetected = true;
-                
-                print("PS4 CONTROLLER IS CONNECTED");
                 controllerSelectionOverlay.SetActive(true); //Displays the controller selection overlay.
                 ps4.SetActive(true);
                 currentInputDevice = inputDevice.pController;
             }
-            else if (names[x].Length == 33)
+            else if (names[x].Length == 33)//33 equals the number of characters in Xbox One controllers Unity name.
             {
                 controllerDetected = true;
-                print("XBOX ONE CONTROLLER IS CONNECTED");
                 controllerSelectionOverlay.SetActive(true); //Displays the controller selection overlay.
                 xbox.SetActive(true);
                 currentInputDevice = inputDevice.xController;
             }
-               
         }
 
         if (!controllerDetected)
         {
-            print("No controller detected!"); // If no controller is connected, the game continues with mouse & keyboard settings.
+            // If no controller is connected, the game continues with mouse & keyboard settings.
             mouseAndKeyboard.SetActive(true);
             currentInputDevice = inputDevice.noController;
             return;
