ec98400a76ab10d9df5a88162e10880bf1b65591
Jonas96Lundin <62259168+Jonas96Lundin@users.noreply.github.com>
2020-05-27 10:32:29 +0200

    Old working version of Game

---
+++ DiscoCube/Assets/Scripts/LevelCodeInput.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LevelCodeInput : MonoBehaviour
+{
+    [SerializeField]
+    SceneFader fader;
+    [SerializeField]
+    GameObject inputField;
+
+    void OnEnable()
+    {
+        //Clear selected object in event system.
+        EventSystem.current.SetSelectedGameObject(null);
+        //Set a new object in event system.
+        EventSystem.current.SetSelectedGameObject(inputField);
+    }
+    public void CheckCode(string input)
+    {
+        switch (input.ToLower())
+        {
+            //Level2
+            case "12345678":
+                fader.FadeTo("Level2");
+                break;
+            //Level3
+            case "nolimits":
+                fader.FadeTo("Level3");
+                break;
+            //Level4
+            case "thewalls":
+                fader.FadeTo("Level4");
+                break;
+            //Level5
+            case "morecube":
+                fader.FadeTo("Level5");
+                break;
+            //Level6
+            case "notriang":
+                fader.FadeTo("Level6");
+                break;
+            //Level7
+            case "cubelord":
+                fader.FadeTo("Level7");
+                break;
+            default:
+                break;
+        }
+    }
+}
